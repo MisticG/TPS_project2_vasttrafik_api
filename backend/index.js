d2fdc9a4e148820c4339910486e0a3f79e34acde
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-12-16 11:09:21 +0100

    currently testing index

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,22 @@
 exports.__esModule = true;
 var express = require("express");
 var bodyParser = require("body-parser");
-var authenticationHandler_1 = require("./authenticationHandler");
+//import { token } from './authenticationHandler';
+var authenticationHandler_1 = require("./handlers/authenticationHandler");
+var saveLocationHandler_1 = require("./handlers/saveLocationHandler");
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function (req, res) {
-    res.send(authenticationHandler_1.authenticate());
+app.use('/', authenticationHandler_1.handleAuthenticate.authenticate);
+app.use('/', function (req, res, next) {
+    var intervalId = setInterval(function () {
+        saveLocationHandler_1.locationHandler.saveAllLocation(req, res, next);
+    }, 10000);
+    //next() 
+});
+//app.use('/', locationHandler.saveAllLocation)
+app.get('/', function (req, res, next) {
+    res.send({ token: res.locals.token });
 });
 var port = 5000;
 app.listen(port, function () { return console.log("Listening on port " + (process.env.PORT || port)); });
--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,29 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { authenticate } from './authenticationHandler';
+//import { token } from './authenticationHandler';
+import { handleAuthenticate } from './handlers/authenticationHandler';
+import { locationHandler } from './handlers/saveLocationHandler';
 
 const app = express();
+
+handleAuthenticate.authenticate;
+setInterval(locationHandler.saveAllLocation, 1000 * 60 * 60 * 24)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/',(req:express.Request, res:express.Response)=>{
-    res.send(authenticate())
+app.use('/', handleAuthenticate.authenticate);
+//app.use('/', locationHandler.saveAllLocation)
+
+app.use('/searchJourney')
+
+app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+    res.send({token: res.locals.token})
 })
 
-
-
+/* 
+Jag ringer på slack
+ */
 const port = 5000;
 app.listen(port, () => console.log(`Listening on port ${ process.env.PORT||port }`));
